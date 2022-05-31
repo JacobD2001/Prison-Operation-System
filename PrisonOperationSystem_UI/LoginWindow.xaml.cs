de2e41c5d176fc4d9ee167915144d3bbfe233e1b
Jakub Dzikowski <Jakub.Dzikowski@microsoft.wsei.edu.pl>
2022-05-31 17:27:08 +0200

    login window finished,added logic for login button

--- PrisonOperationSystem_UI/LoginWindow.xaml.cs
+++ PrisonOperationSystem_UI/LoginWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,6 +24,40 @@ namespace PrisonOperationSystem_UI
         public LoginWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            SqlConnection connection = new SqlConnection(@"Data Source=DESKTOP-2V1OQNV;Initial Catalog=ZakładKarny;Integrated Security=True");
+            try
+            {
+                if(connection.State == ConnectionState.Closed)
+                    connection.Open();
+                string query = "SELECT (1) FROM DaneLogowania WHERE Login=@Login AND Haslo=@Haslo";
+                SqlCommand cmd = new SqlCommand(query, connection); //runs query on connected database
+                cmd.CommandType = CommandType.Text;
+                cmd.Parameters.AddWithValue("@Login", txtLogin.Text); //declare scalar variable
+                cmd.Parameters.AddWithValue("@Haslo", txtHaslo.Password);
+                int count = Convert.ToInt32(cmd.ExecuteScalar()); 
+                if (count == 1) //if it returns 1(meaning 1 of the rows in db) go to next window
+                {
+                    MainWindow Dashboard = new MainWindow();
+                    Dashboard.Show();
+                    this.Close();
+                }
+                else //else display this one
+                {
+                    MessageBox.Show("Login lub hasło jest niepoprawne!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
         }
     }
 }
