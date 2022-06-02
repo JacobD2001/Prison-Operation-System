991c1bdb6f320e814af968c1654284891041dfa8
Jakub Dzikowski <Jakub.Dzikowski@microsoft.wsei.edu.pl>
2022-06-02 14:02:34 +0200

    added new textboxes, added logic for textboxes under combobox

--- PrisonOperationSystem_UI/MainWindow.xaml.cs
+++ PrisonOperationSystem_UI/MainWindow.xaml.cs
@@ -28,7 +28,7 @@ namespace PrisonOperationSystem_UI
             FillComboBox();
         }
 
-        public void FillComboBox()
+        public void FillComboBox() //filling combobox with data
         {
             SqlConnection connection = new SqlConnection(@"Data Source=DESKTOP-2V1OQNV;Initial Catalog=ZakładKarny;Integrated Security=True");
             try
@@ -54,14 +54,15 @@ namespace PrisonOperationSystem_UI
             }
         }
 
-        private void ComboBox_DropDownClosed(object sender, EventArgs e)
+        private void ComboBox_DropDownClosed(object sender, EventArgs e) //adding logic for combobox(filling textboxes with data acording to PESEL)
         {
             SqlConnection connection = new SqlConnection(@"Data Source=DESKTOP-2V1OQNV;Initial Catalog=ZakładKarny;Integrated Security=True");
             try
             {
-                if (connection.State == ConnectionState.Closed)
-                    connection.Open();
+                if (connection.State == ConnectionState.Closed) 
+                    connection.Open();                               //TODO UPTADE QUERY 
                 string query = "SELECT * FROM Wiezniowie WHERE PESEL='" + DisPESEL.Text + "' ";
+                
                 SqlCommand command = new SqlCommand(query, connection);
                 SqlDataReader dr = command.ExecuteReader();
                 while (dr.Read())
@@ -69,14 +70,26 @@ namespace PrisonOperationSystem_UI
                     string imie = dr.GetString(1);
                     string nazwisko = dr.GetString(2);
                     string PESEL = dr.GetString(3);
-                    string RozpWyroku = dr.GetDateTime(4).ToString();
-                    string ZakWyroku = dr.GetDateTime(5).ToString();
+                    string rozpWyroku = dr.GetDateTime(4).ToString();
+                    string zakWyroku = dr.GetDateTime(5).ToString();
+                    string przestepstwo = dr.GetInt32(11).ToString();
+                    string stZagro = dr.GetInt32(12).ToString();
+                    string cela = dr.GetInt32(13).ToString();
 
                     txtimie.Text = imie;
                     txtnazwisko.Text = nazwisko;
                     txtPESEL.Text = PESEL;
-                    txtRozpWyroku.Text = RozpWyroku;
-                    txtZakWyroku.Text = ZakWyroku;
+                    txtRozpWyroku.Text = rozpWyroku;
+                    txtZakWyroku.Text = zakWyroku;
+                    //TODO Display actual data not relation(3 V)
+                    txtConvictedFor.Text = przestepstwo;
+                    txtThreatLevel.Text = stZagro;
+                    txtCell.Text = cela;
+                    
+                   
+                    
+                    
+                    
                 }
             }
             catch (Exception ex)
@@ -88,6 +101,52 @@ namespace PrisonOperationSystem_UI
             {
                 connection.Close();
             }
+
+
+
+        }
+
+        private void btnAddPrisoner_Click(object sender, RoutedEventArgs e) //logic for addprisoner button
+        {
+            SqlConnection connection = new SqlConnection(@"Data Source=DESKTOP-2V1OQNV;Initial Catalog=ZakładKarny;Integrated Security=True");
+            try
+            {
+                if (connection.State == ConnectionState.Closed)
+                    connection.Open();
+                string query = "INSERT INTO Wiezniowie (Imie,Nazwisko,PESEL,RozpoczecieWyroku,PlanowaneZakonczenieWyroku) VALUES('"+this.txtimie.Text+ "','"+this.txtnazwisko.Text+"','"+this.txtPESEL.Text+"','"+this.txtRozpWyroku.Text+"','"+this.txtZakWyroku.Text+"')";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.ExecuteNonQuery();
+                MessageBox.Show("Prisoner has been added");
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
